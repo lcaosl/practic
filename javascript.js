7cdd398ec570ddd50a2f1ba294f6cab1f48accb7
Dmitry Kalugin <lcaosl@yandex.ru>
2021-07-15 14:18:29 +0300

    Data type

--- javascript.js
+++ javascript.js
@@ -1,25 +1,39 @@
-let money = prompt("Ваш бюджет на месяц?", ''),
-	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+"use strict";
 
-let appData = {
-	budget: money,
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	timeData: time,
-	savings: false
-};
+// let money = prompt("Ваш бюджет на месяц?", ''),
+// 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-for (let i = 0; i < 2; i++) {
-	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-		b = prompt("Во сколько обойдется?", '');
+// let appData = {
+// 	budget: money,
+// 	expenses: {},
+// 	optionalExpenses: {},
+// 	income: [],
+// 	timeData: time,
+// 	savings: false
+// };
 
-	if ((tipeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
-		&& a != '' && b != '' && a.length < 50) {
+// for (let i = 0; i < 2; i++) {
+// 	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+// 		b = prompt("Во сколько обойдется?", '');
 
-		appData.expenses[a] = b;
-	}
+// 	if ((tipeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
+// 		&& a != '' && b != '' && a.length < 50) {
+
+// 		appData.expenses[a] = b;
+// 	}
 	
-}
+// }
 
-alert(appData.budget / 30);
+// alert(appData.budget / 30);
+
+
+
+let admin;
+let userName;
+
+userName = 'Джон';
+
+admin = userName;
+
+alert(admin);
+
