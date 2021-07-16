32f6001341c8608e299942f6d19bad4c035b1022
Dmitry Kalugin <lcaosl@yandex.ru>
2021-07-16 14:18:33 +0300

    Operarors. if, ?.Second half

--- javascript.js
+++ javascript.js
@@ -1,101 +1,117 @@
 "use strict";
 
-// let money = prompt("Ваш бюджет на месяц?", ''),
-// 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-// let appData = {
-// 	budget: money,
-// 	expenses: {},
-// 	optionalExpenses: {},
-// 	income: [],
-// 	timeData: time,
-// 	savings: false
-// };
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
 
-// for (let i = 0; i < 2; i++) {
-// 	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-// 		b = prompt("Во сколько обойдется?", '');
+for (let i = 0; i < 2; i++) {
+	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+		b = prompt("Во сколько обойдется?", '');
 
-// 	if ((tipeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
-// 		&& a != '' && b != '' && a.length < 50) {
+	if ((tipeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
+		&& a != '' && b != '' && a.length < 50) {
 
-// 		appData.expenses[a] = b;
-// 	}
+		appData.expenses[a] = b;
+	}
 	
-// }
+}
 
-// alert(appData.budget / 30);
-
-
-
-
-
-// let admin;
-// let userName;
-
-// userName = 'Джон';
-
-// admin = userName;
-
-// alert(admin);
+alert(appData.budget / 30);
 
 
 
 
-// let name = prompt('Как твоё имя?', '');
-// alert('Твоё имя ' + name);
 
+let admin;
+let userName;
 
+userName = 'Джон';
 
-// let x = "" - 1 + 0;
-// alert(x);
+admin = userName;
 
-
-// let a = prompt("Первое число?", 1);
-// let b = prompt("Второе число?", 2);
-
-// alert(+a + +b); // 12
+alert(admin);
 
 
 
 
-// let javascriptCreator = prompt('Какое «официальное» название JavaScript?', '');
-
-// if (javascriptCreator == 'ECMAScript') {
-//     alert('Красава!');
-// } else {
-//     alert('«Не знаете? ECMAScript!»');
-// }
+let name = prompt('Как твоё имя?', '');
+alert('Твоё имя ' + name);
 
 
 
-
-// let value = prompt('Ввведите какое-нибудь число', '');
-
-// if (value > 0) {
-//     alert(1);
-// } else if (value < 0) {
-//     alert(-1);
-// } else {
-//     alert(0);
-// }
+let x = "" - 1 + 0;
+alert(x);
 
 
-//----------------------------------------------------------------------------
-// let result;
+let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
 
-// if (a + b < 4) {
-//   result = 'Мало';
-// } else {
-//   result = 'Много';
-// }
+alert(+a + +b); // 12
 
-// Ниже через '?'
+
+
+
+let javascriptCreator = prompt('Какое «официальное» название JavaScript?', '');
+
+if (javascriptCreator == 'ECMAScript') {
+    alert('Красава!');
+} else {
+    alert('«Не знаете? ECMAScript!»');
+}
+
+
+
+
+let value = prompt('Ввведите какое-нибудь число', '');
+
+if (value > 0) {
+    alert(1);
+} else if (value < 0) {
+    alert(-1);
+} else {
+    alert(0);
+}
+
+
+----------------------------------------------------------------------------
+let result;
+
+if (a + b < 4) {
+  result = 'Мало';
+} else {
+  result = 'Много';
+}
+
+Ниже через '?'
 
 let result = (a + b < 4) ? 'Мало' : 'Много';
 
 
-//----------------------------------------------------------------------------
+----------------------------------------------------------------------------
+let message;
 
+if (login == 'Сотрудник') {
+  message = 'Привет';
+} else if (login == 'Директор') {
+  message = 'Здравствуйте';
+} else if (login == '') {
+  message = 'Нет логина';
+} else {
+  message = '';
+}
+Переделаем через "?"
 
+let message = (login == 'Сотрудник') ? 'Привет':
+    (login == 'Директор') ? 'Здравствуйте':
+    (login == '') ? 'Нет логина':
+    '';
 
+---------------------------------------------------------------------------
